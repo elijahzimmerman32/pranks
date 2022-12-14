9f92fa89545a7235dfa6a3971db67915d15c4298
elijahzimmerman32 <96357379+elijahzimmerman32@users.noreply.github.com>
2022-12-14 11:58:55 -0500

    Add files via upload

---
+++ rickroll.py
@@ -0,0 +1,16 @@
+import webbrowser
+import os
+
+url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+
+# MacOS
+chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+
+# Windows
+# chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+
+# Linux
+# chrome_path = '/usr/bin/google-chrome %s'
+
+webbrowser.get(chrome_path).open(url)
+
